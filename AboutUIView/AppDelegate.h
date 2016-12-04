b6309530f67d8d89989c45e815e32a1d4516c30c
binbin <2426715336@qq.com>
2016-12-04 11:31:24 +0800

    Initial Commit

---
+++ AboutUIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AboutUIView
+//
+//  Created by mac on 16/12/4.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AboutUIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AboutUIView
+//
+//  Created by mac on 16/12/4.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
